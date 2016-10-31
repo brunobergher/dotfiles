9a37efc6fdd32694b7c75fc6859296b57d29c821
Bruno Bergher <me@brunobergher.com>
2016-10-30 23:40:57 -0700

    Git updates

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,4 +1,6 @@
+alias gad="git add"
 alias gal="git add . && git add -u && echo -e \"\033[42m Added all changes: \033[40m\" && gst"
+alias gam="git commit --amend"
 alias gbr="git branch"
 alias gcl="git clean -fd"
 alias gco="git checkout"
