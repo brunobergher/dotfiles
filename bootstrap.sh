ae0bc22d3a529cb5b2e25f5604677e391e3c6413
Bruno Bergher <me@brunobergher.com>
2016-10-31 01:21:32 -0700

    More bootstrap cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,19 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "init.sh" \
-    --exclude "aliases/" --exclude "sublime/"  --exclude "init/" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" \
+		--exclude "bootstrap.sh" --exclude "init.sh" \
+		--exclude "aliases/" --exclude "sublime/"  --exclude "init/" \
+		--exclude "Brewfile" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" \
+		-av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
@@ -19,4 +23,4 @@ fi
 unset doIt
 
 echo
-echo "If you're setting up a new Mac, you may want to run ./init.sh"
+echo "▶︎ If you're setting up a new Mac, you may want to run ./init.sh"
