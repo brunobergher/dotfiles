525ff8734dd56a1a4b3f50b9c709e2a34ba166e0
Bruno Bergher <me@brunobergher.com>
2016-11-04 19:02:54 -0700

    Video togif script

--- scripts/video/togif.rb
+++ scripts/video/togif.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/env ruby
 
 # Usage
-# togif WIDTH FPS
+# togif FILE WIDTH FPS
 
-width = ARGV[0].to_i
-fps   = ARGV[1].to_i
+if ARGV[0].nil?
+  puts "\e[32mUsage: togif FILE WIDTH FPS\e[m"
+  exit
+end
 
-puts "\e[32mUsage: togif WIDTH FPS\e[m"
-puts "TODO: Implement"
+file  = ARGV[0]
+width = (ARGV[1] || 400).to_i
+fps   = (ARGV[2] || 20).to_i
+output = File.basename(file, ".*") + ".gif"
+
+puts "Converting #{file} into #{output} at #{width} pixels wide and #{fps} fps..."
+
+palette = "/tmp/togif-temp-palette.png"
+filters = "fps=#{fps},scale=#{width}:-1:flags=lanczos"
+
+File.unlink(palette) if File.exists?(palette)
+
+system "ffmpeg -v error -i #{file} -vf \"#{filters},palettegen\" -y #{palette}"
+system "ffmpeg -v error -i #{file} -i #{palette} -lavfi \"#{filters} [x]; [x][1:v] paletteuse\" -y #{output}"
