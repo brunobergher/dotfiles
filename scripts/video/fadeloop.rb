aa1b1e3f9c8940880c6a9d495e47d99513ff5485
Bruno Bergher <me@brunobergher.com>
2017-09-17 10:16:24 -0700

    Update to fadeloop script

--- scripts/video/fadeloop.rb
+++ scripts/video/fadeloop.rb
@@ -2,6 +2,7 @@
 
 if ARGV[0].nil?
   puts "\e[32mUsage: fadeloop FILE FADELENGTH\e[m"
+  puts "\e[32m  FADELENGTH is in seconds. Default is 1\e[m"
   exit
 end
 
@@ -16,7 +17,7 @@ puts "Creating loopable video of #{input} with #{fade}s crossfade..."
 cmd_len = "ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 #{input}"
 length = Float(`#{cmd_len}`)
 
-cmd_conv = "ffmpeg -i #{input} -filter_complex \"[0]split[body][pre];[pre]trim=duration=#{fade},format=yuva420p,fade=d=#{fade}:alpha=1,setpts=PTS+(#{length-fade}/TB)[jt];[body]trim=0,setpts=PTS-STARTPTS[main];[main][jt]overlay\" #{output}"
+cmd_conv = "ffmpeg -i #{input} -filter_complex \"[0]split[body][pre];[pre]trim=duration=#{fade},format=yuva420p,fade=duration=#{fade}:alpha=1,setpts=PTS+(#{length-fade-fade}/TB)[jt];[body]trim=0,setpts=PTS-STARTPTS[main];[main][jt]overlay\" #{output}"
 system cmd_conv
 
 puts "Created #{output}, #{length}s long."
