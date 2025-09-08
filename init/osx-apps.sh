4afb1eed5444de859e6d2012ddc739caf0e1bb4b
Bruno Bergher <me@brunobergher.com>
2025-09-08 13:04:58 +0100

    Updates cask app list

--- init/osx-apps.sh
+++ init/osx-apps.sh
@@ -3,10 +3,15 @@ brew install  --cask zen
 brew install  --cask raycast
 brew install  --cask todoist
 brew install  --cask 1password
-brew install  --cask vscode
+brew install  --cask joplin
 
 brew install  --cask signal
 brew install  --cask spotify
 brew install  --cask whatsapp
 
 brew install  --cask caffeine
+brew install  --cask menubarclock
+
+brew install  --cask vscode
+brew install  --cask docker
+brew install  --cask figma
