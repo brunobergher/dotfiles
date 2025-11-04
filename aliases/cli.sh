6933dae76542f7ba3a62881704fb3d613c5ac973
Bruno Bergher <me@brunobergher.com>
2025-11-04 14:53:52 +0000

    Adds killport command

--- aliases/cli.sh
+++ aliases/cli.sh
@@ -11,3 +11,21 @@ alias sudo='sudo '
 # Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
 # (useful when executing time-consuming commands)
 alias badge="tput bel"
+
+killport() {
+  PORT=$1
+  if [ -z "$PORT" ]; then
+    echo "Please specify a port number."
+    return 1
+  fi
+
+  PID=$(lsof -t -i:$PORT)
+
+  if [ -z "$PID" ]; then
+    echo "No process found running on port $PORT."
+  else
+    echo "Killing process with PID $PID on port $PORT."
+    kill -9 "$PID"
+    echo "Process killed."
+  fi
+}
